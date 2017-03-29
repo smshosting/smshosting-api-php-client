b1a4f6a6f71fa8baaada10198f1f9a82d7ebd003
Denis Tollot <d.tollot@i-contact.it>
2017-03-29 08:46:15 +0200

    added phonebook's requests

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Il progetto contiene un esempio di client, sviluppato in PHP, per le REST API di Smshosting.
 
-La documentazione completa delle API  è disponibile all'indirizzo [https://www.smshosting.it/it/docs/sms-rest-api/introduzione](https://www.smshosting.it/it/docs/sms-rest-api/introduzione).
+Il client si basa sul framework [http://unirest.io/](unirest) e contiene, a scopo esemplificativo, alcune delle chiamate disponibili nelle API di Smshosting. 
+
+La documentazione completa delle API è disponibile all'indirizzo [https://www.smshosting.it/it/docs/sms-rest-api/introduzione](https://www.smshosting.it/it/docs/sms-rest-api/introduzione).
+
+Per ogni chiamata nella documentazione troverete degli snippet di codice (sempre basati su [http://unirest.io/](unirest)) da integrare nel vosto client.
--- client/src/SmsHostingClient.php
+++ client/src/SmsHostingClient.php
@@ -29,39 +29,110 @@ class SmsHostingClient {
     ## send sms requests ##
 
     public function sendSms($params = array()) {
-        
+
         $body = \Unirest\Request\Body::form($params);
 
         return \Unirest\Request::post(self::BASE_REST_API . 'sms/send', array(), $body);
     }
-    
+
     public function sendSmsBulk($params = array()) {
-        
+
         $body = \Unirest\Request\Body::form($params);
 
         return \Unirest\Request::post(self::BASE_REST_API . 'sms/sendbulk', array(), $body);
     }
-    
+
     public function estimateSendSms($params = array()) {
-        
+
         $body = \Unirest\Request\Body::form($params);
 
         return \Unirest\Request::post(self::BASE_REST_API . 'sms/estimate', array(), $body);
     }
-    
+
     public function cancelSms($params = array()) {
-        
+
         $body = \Unirest\Request\Body::form($params);
 
         return \Unirest\Request::post(self::BASE_REST_API . 'sms/cancel', array(), $body);
     }
-    
+
     public function searchSms($params = array()) {
-        
+
         return \Unirest\Request::get(self::BASE_REST_API . 'sms/search', array(), $params);
     }
 
+    ## sms received requests ##
 
+    public function searchSmsReceived($params = array()) {
+
+        return \Unirest\Request::get(self::BASE_REST_API . 'sms/received/search', array(), $params);
+    }
+
+    public function getSimForReceiveSmsList() {
+        return \Unirest\Request::get(self::BASE_REST_API . 'sms/received/sim/list');
+    }
+
+    ## phonebook groups requests ##
+
+    public function getGroupList() {
+        return \Unirest\Request::get(self::BASE_REST_API . 'phonebook/group/list');
+    }
+
+    public function getGroup($idGroup) {
+        return \Unirest\Request::get(self::BASE_REST_API . "phonebook/group/" . $idGroup);
+    }
+
+    public function getGroupContacts($idGroup, $params = array()) {
+        return \Unirest\Request::get(self::BASE_REST_API . "phonebook/group/" . $idGroup . "/contacts", array(), $params);
+    }
+
+    public function addGroup($params = array()) {
+
+        $body = \Unirest\Request\Body::form($params);
+
+        return \Unirest\Request::post(self::BASE_REST_API . 'phonebook/group', array(), $body);
+    }
+
+    public function updateGroup($idGroup, $params = array()) {
+
+        $body = \Unirest\Request\Body::form($params);
+
+        return \Unirest\Request::post(self::BASE_REST_API . 'phonebook/group/' . $idGroup, array(), $body);
+    }
+
+    public function deleteGroup($idGroup, $deleteContacts = false) {
+
+        return \Unirest\Request::delete(self::BASE_REST_API . 'phonebook/group/' . $idGroup . '?delete_contacts=' . $deleteContacts);
+    }
+
+    ## phonebook contacts requests ##
+
+    public function searchContacts($params = array()) {
+        return \Unirest\Request::get(self::BASE_REST_API . 'phonebook/contact/search', array(), $params);
+    }
+
+    public function getContact($msisdn) {
+        return \Unirest\Request::get(self::BASE_REST_API . "phonebook/contact/" . $msisdn);
+    }
+
+    public function addContact($params = array()) {
+
+        $body = \Unirest\Request\Body::form($params);
+
+        return \Unirest\Request::post(self::BASE_REST_API . 'phonebook/contact', array(), $body);
+    }
+
+    public function updateContact($msisdn, $params = array()) {
+
+        $body = \Unirest\Request\Body::form($params);
+
+        return \Unirest\Request::post(self::BASE_REST_API . 'phonebook/contact/' . $msisdn, array(), $body);
+    }
+
+    public function deleteContact($msisdn) {
+
+        return \Unirest\Request::delete(self::BASE_REST_API . 'phonebook/contact/' . $msisdn);
+    }
 
     ## user requests ##
 
